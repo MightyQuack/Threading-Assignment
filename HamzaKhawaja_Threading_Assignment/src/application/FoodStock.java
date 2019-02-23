b54bf422b0d9eed01a07b0510d072b6421e1ec54
Hamza <khawaham@sheridancollege.ca>
2019-02-23 16:53:26 -0500

    Fixed Import

--- HamzaKhawaja_Threading_Assignment/src/application/FoodStock.java
+++ HamzaKhawaja_Threading_Assignment/src/application/FoodStock.java
@@ -1,6 +1,5 @@
 package application;
 
-import static application.Zoo.numOfFeeds;
 import static java.lang.System.exit;
 import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.Lock;
@@ -21,7 +20,7 @@ public class FoodStock extends Zoo {
 		return totalFoodEaten;
 	}
 
-	// Params amount is the amount of food animal is needed to eat and animal is the
+	// Parameter amount is the amount of food animal is needed to eat and animal is the
 	// animal object to be passed
 	public void feed(int amount, Animal animal) {
 		lock.lock();
@@ -40,6 +39,7 @@ public class FoodStock extends Zoo {
 					animal.setFoodEaten(amount);
 				}
 			}
+			
 			if (numOfFeeds == 10) {
 				System.out.println("Finished Tasks");
 				// Perform database operations
